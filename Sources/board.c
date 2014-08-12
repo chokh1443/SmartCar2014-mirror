515a80bb02b24d2fde709875ea04719e7e23dae0
bluemir <qa22ahj@gmail.com>
2014-08-12 20:15:43 +0900

    update gpio

--- Sources/board.c
+++ Sources/board.c
@@ -1,32 +1,48 @@
 #include "board.h"
 #include "jdp.h"
+#include "zp_pin.h"
 
-void LED_ON(uint16_t number);
-void LED_OFF(uint16_t number);
+void LED_ON(uint16_t ledId);
+void LED_OFF(uint16_t ledId);
 void GPIO_ON(uint16_t ch);
 void GPIO_OFF(uint16_t ch);
 uint8_t GPIO_GET(uint16_t ch);
 
+const uint8_t CONST_SEGMENT_PIN_NUMBER[3][4] = {
+	{ DO_SEG1_A, DO_SEG1_B, DO_SEG1_C, DO_SEG1_D },
+	{ DO_SEG2_A, DO_SEG2_B, DO_SEG2_C, DO_SEG2_D },
+	{ DO_SEG3_A, DO_SEG3_B, DO_SEG3_C, DO_SEG3_D }
+};
+const uint8_t CONST_SEGMENT_PIN_ENABLE[3][4] = {
+	{ DO_SEG1_1, DO_SEG1_2, DO_SEG1_3, DO_SEG1_4 },
+	{ DO_SEG2_1, DO_SEG2_2, DO_SEG2_3, DO_SEG2_4 },
+	{ DO_SEG3_1, DO_SEG3_2, DO_SEG3_3, DO_SEG3_4 }
+};
+
 void initBoard(Board * board) {
+	//default I/O functions
 	board->led.on = LED_ON;
 	board->led.off = LED_OFF;
 
 	board->gpio.on = GPIO_ON;
 	board->gpio.off = GPIO_OFF;
 	board->gpio.get = GPIO_GET;
-}
-void LED_ON(uint16_t number) {
 	
+	board->gpio.ch.segmentNumber = (uint8_t**)CONST_SEGMENT_PIN_NUMBER;
+	board->gpio.ch.segmentEnable = (uint8_t**)CONST_SEGMENT_PIN_ENABLE;
 }
-void LED_OFF(uint16_t number) {
-	
+void LED_ON(uint16_t ledId) {
+	GPIO_ON(DO_LED1 + ledId);
+}
+void LED_OFF(uint16_t ledId) {
+	GPIO_OFF(DO_LED1 + ledId);
 }
 void GPIO_ON(uint16_t ch) {
-	SIU.GPDO[ch].B.PDO = 1;
-}
-void GPIO_OFF(uint16_t ch) {
 	SIU.GPDO[ch].B.PDO = 0;
 }
+void GPIO_OFF(uint16_t ch) {
+	SIU.GPDO[ch].B.PDO = 1;
+}
 uint8_t GPIO_GET(uint16_t ch) {
-	return (uint8_t) SIU.GPDI[ch].B.PDI;
+	return (uint8_t) SIU .GPDI[ch].B.PDI;
 }
--- Sources/board.h
+++ Sources/board.h
@@ -1,15 +1,21 @@
 #pragma once
 #include "typedefs.h"
+#include "zp_pin.h"
 
 typedef struct {
-	void (*on)(uint16_t number);
-	void (*off)(uint16_t number);
+	void (*on)(uint16_t ledId);
+	void (*off)(uint16_t ledId);
 } LED;
 
 typedef struct {
-	void (*on)(uint16_t number);
-	void (*off)(uint16_t number);
-	uint8_t (*get)(uint16_t number);
+	void (*on)(uint16_t ch);
+	void (*off)(uint16_t ch);
+	uint8_t (*get)(uint16_t ch);
+	
+	struct {
+		uint8_t ** segmentNumber;
+		uint8_t ** segmentEnable;
+	} ch;
 } GPIO;
 
 typedef struct {
@@ -18,3 +24,4 @@ typedef struct {
 } Board;
 
 void initBoard(Board * board);
+
