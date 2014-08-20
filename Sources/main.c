eb2e4e9c9ceb742dae9f5c637908d63b2506a712
bluemir <qa22ahj@gmail.com>
2014-08-20 23:45:18 +0900

    add binarization

--- Sources/main.c
+++ Sources/main.c
@@ -10,18 +10,13 @@
 #include "smartcar.h"
 #include "gpio_drv.h"
 #include "test.h"
+#include "utils.h"
 
 /**********************  Function Prototype here *************************/
 
 void main(void);
 void preset(SmartCar * smartCar);
 void start(SmartCar * smartCar);
-
-void testServo(Servo * servo, Segment * segment, Segment * step);
-void set_Line(uint16_t inputLine[2][128], uint16_t setLine[2][128]);
-void fix_Line(uint16_t line[2][128]);
-void insert_Line(uint16_t input[2][128], uint16_t map[10][2][128]);
-void check_direction(uint16_t standard[2][128], uint16_t map[10][2][128]);
 
 unsigned char newLine[2] = {'\r','\n'};
 
@@ -30,7 +25,6 @@ unsigned char newLine[2] = {'\r','\n'};
 
 void main(void) {
 	SmartCar smartCar;
-	LEDData data;
 	sys_init_fnc();
 	EnableExternalInterrupts();
 	
@@ -65,23 +59,72 @@ void main(void) {
 //camera inputing	time = 0.04~0.06s(about 50ms)
 //servo motor moving time = 0.2s(200ms)
 //we have about 4 data for moving servo motor
-//make map useing 5 camera data
+//make map using 5 camera data
+
+typedef struct {
+	int16_t data[128];
+	Range range;
+} DifferentialData;
+
+void getDifferentialData(uint16_t * data, DifferentialData * result);
+void binarization(DifferentialData * differentialData, uint8_t * result);
+
+int8_t findLine(Camera * camera);
 
 void start(SmartCar * smartCar){
 	uint8_t i, j;					// 0 left, 1 right
 	
 	while(1){
+		int8_t pos[2];
+		pos[0] = findLine(&smartCar->camera[0]);
+		pos[1] = findLine(&smartCar->camera[1]);
 		
 	}
 }
 
-void findLine(Camera * camera){
-	uint16_t i;
-	uint16_t * data = Camera_get(camera);
+int8_t findLine(Camera * camera){
 	
-	uint16_t value = data[0];
+	uint16_t * data = Camera_get(camera);
+	DifferentialData differentialData;
+	uint8_t result[128];
+	
+	getDifferentialData(data, &differentialData);
+	binarization(&differentialData, result);
+	
+}
+void getDifferentialData(uint16_t * data, DifferentialData * result){
+	int16_t min = 0, max = 0;
+	register uint16_t i;
+	
+	result->data[0] = data[0];
 	for(i = 1; i < 128; i++){
-		if(data[i] - data[i-1] > 400){ //XXX			
+		result->data[i] = data[i-1] - data[i];
+		
+		if(min > result->data[i]){
+			min = result->data[i];
+		}
+		if(max < result->data[i]){
+			max = result->data[i];
+		}
+	}
+	result->range.low = min;
+	result->range.high = max;
+}
+void binarization(DifferentialData * differentialData, uint8_t * result){
+	int16_t lowCrit = differentialData->range.low/2;
+	int16_t highCrit = differentialData->range.high/2;
+	int16_t * diffedData = differentialData->data;
+	
+	register uint16_t i;
+
+	result[0] = 0;
+	for(i = 1; i < 128; i++){
+		if(diffedData[i] > highCrit) {
+			result[i] = 0;
+		} else if(diffedData[i] < lowCrit){
+			result[i] = 1;
+		} else {
+			result[i] = result[i-1];
 		}
 	}
 }
@@ -89,27 +132,3 @@ void findLine(Camera * camera){
 void preset(SmartCar * smartCar){
 	
 }
-
-//setLine
-void set_Line(uint16_t inputLine[2][128], uint16_t setLine[2][128]){
-	uint8_t i, j;
-	uint16_t standardOfcolor=0; 		// standard for checking color.
-	
-	for(i=0;i<2;i++){
-		for(j=0;j<128;j++){
-			standardOfcolor = standardOfcolor + inputLine[i][j]; 
-		}
-		
-		standardOfcolor = standardOfcolor / 128;
-		
-		for(j=0;j<128;j++){
-			if(inputLine[i][j] >= standardOfcolor){
-				setLine[i][j] = 0; // white(out or road)
-			} else{
-				setLine[i][j] = 1; // black(line)
-			}	
-		}
-	}
-	fix_Line(setLine);
-}
-
---
+++ Sources/utils.h
@@ -0,0 +1,6 @@
+#pragma once
+
+typedef struct {
+	int32_t low;
+	int32_t high;
+} Range;
