c13fd9ce6ee22c0565a790c8fa23c3a42ecf5c5d
chokh1443 <chokh1443@gmail.com>
2014-08-21 04:58:21 +0900

    Motor_PID

--- Sources/motor.c
+++ Sources/motor.c
@@ -30,11 +30,30 @@ void Motor_setki(Motor * this, int32_t val){
 void Motor_setkd(Motor * this, int32_t val){
 	this->kd = val;
 }
+void Motor_addErr(Motor * this, int32_t newErr) {
+	this->err[0] = this->err[1];//n-2
+	this->err[1] = this->err[2];//n-1
+	this->err[2] = newErr;//n
+	
+}
+int32_t Motor_PID(Motor * this){//FIX ME!!
+	int32_t newErr = 0;
+	int32_t encoderSpeed = 0;
+	int32_t deltaSpeed = 0;
+	
+	newErr = targetEncoderSpeed - realEncoderSeped;//FIX ME
 
+	Motor_addErr(this , newErr);
+	
+	deltaSpeed = this->kp*(this->err[2]- this->err[1])
+			+ this->ki*(this->err[2])
+			+ this->kd*(this->err[2] - 2*this->err[1] + this->err[0]);
+	
+	deltaSpeed /= 1000;
+	
+	return deltaSpeed;
+}
 void Motor_pidTick(Motor * this){
-	int32_t kp = this->kp;
-	int32_t ki = this->ki;
-	int32_t kd = this->kd;
 
 	//int16_t speed = Encoder_get(this->encoder);
 
--- Sources/motor.h
+++ Sources/motor.h
@@ -10,6 +10,8 @@ typedef struct {
 	int32_t kp;
 	int32_t ki;
 	int32_t kd;
+	
+	int32_t err[3];
 } Motor;
 
 void Motor_init(Motor * this);
@@ -19,4 +21,6 @@ void Motor_Disable(Motor * this);
 void Motor_setkp(Motor * this, int32_t val);
 void Motor_setki(Motor * this, int32_t val);
 void Motor_setkd(Motor * this, int32_t val);
+void Motor_addErr(Motor * this, int32_t newErr);
+int32_t Motor_PID(Motor * this);
 void Motor_pidTick(Motor * this);
